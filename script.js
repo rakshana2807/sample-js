b468770e0b986c21c530f843cc5a0d496634b5b6
rakshana2807 <rakshanarajagopal93@gmail.com>
2026-03-04 13:51:12 +0530

    js-level-1-task

--- script.js
+++ script.js
@@ -1,56 +1,37 @@
 
-const input1 = document.getElementById("inp1")
-const input2 = document.getElementById("inp2")
-const container = document.getElementById("container")
-
-// Array to store issued cart
-let cartList = []
-let originalValue = null 
+var input1 = document.getElementById("inp1")
+var input2 = document.getElementById("inp2")
+var container = document.getElementById("container")
+var total= document.getElementById("tot")
+var currenttotal=0
+var cartList = []
 
 function add() {
 
-    let itemName = input1.value.trim()
-    let price = input2.value.trim()
+    var itemName = input1.value.trim()
+    var price = Number(input2.value.trim())
 
     if (itemName === "" || price === "") {
         alert("Please enter details")
         return
     }
 
-    if (cartList.includes(itemName)) {
-        alert("Already exist")
-        return
-    }
 
-    // Store in array
     cartList.push(itemName,price)
 
-    // Check if already exists
-
-
-    // Create list item
-    let list = document.createElement("li")
-    list.innerHTML = itemName + " - " + price + "₹" + "<button onclick='increase()'>+</button>" + "<button onclick='del(event)'>Remove</button>"
+    var list = document.createElement("li")
+    list.innerHTML = itemName + " - " + price + "₹"  + "<button onclick='del(event, " + price + ")'>Remove</button>"
     container.append(list)
-
-    // Clear inputs
+    currenttotal += price
+    total.textContent="Total Price : " + currenttotal
     input1.value = ""
-    input2.value = ""
+    input2.value= ""
 
 }
-function del(event){
+ 
+function del(event,price){
+    
     event.target.parentElement.remove()
-}
-function increase() {
-
-    let currentValue = price
-
-    // If first time clicking
-    if (originalValue === null) {
-        originalValue = currentValue
-    }
-
-    currentValue = currentValue + originalValue
-
-    input2.value = currentValue
+    currenttotal -= price
+    total.textContent="Total Price : " + currenttotal
 }
